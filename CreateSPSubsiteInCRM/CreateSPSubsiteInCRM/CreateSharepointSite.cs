7d65c80f438fcf4dae167cffc8ccc83ca0702c47
suleimanali <suleiman.o.ali@gmail.com>
2018-07-18 11:16:00 -0400

    Create SharePoint subsite from CRM then associate it to a record.

---
+++ CreateSPSubsiteInCRM/CreateSPSubsiteInCRM/CreateSharepointSite.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.Xrm.Sdk;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace CreateSPSubsiteInCRM
+{
+    internal class CreateSharepointSite
+    {
+        internal static bool CreateSubsite(IOrganizationService service, string siteName, string urlString)
+        {
+            var siteUrl = urlString; //@"sharepointUrl";
+            var _username = @"crmuserName";
+            var _password = "crmPassword";
+            var spSite = new Uri(siteUrl);
+            var restURL = siteUrl + "/_api/web/webs/add";
+
+            var stringData = string.Concat("{'parameters': { '__metadata': { 'type': 'SP.WebCreationInformation' },",
+                "'Title': '" + siteName + "', 'Url': '" + siteName + "', 'WebTemplate': '{611CC7DA-4333-4781-BB67-4AD23D0AD267}#HuttonCRMProjectTemplate', 'UseSamePermissionsAsParentSite': true } }");
+
+            var endpointRequest = (HttpWebRequest)HttpWebRequest.Create(restURL);
+            var spo = SpoAuthUtility.Create(spSite, _username, WebUtility.HtmlEncode(_password), false);
+            var cookies = spo.GetCookieContainer();
+            var formDigest = spo.GetRequestDigest();
+            endpointRequest.CookieContainer = cookies;
+            endpointRequest.Method = "POST";
+            endpointRequest.ContentLength = stringData.Length;
+            endpointRequest.Accept = "application/json;odata=verbose";
+            endpointRequest.ContentType = "application/json;odata=verbose";
+            endpointRequest.Headers.Add("X-RequestDigest", formDigest);
+
+            var writer = new StreamWriter(endpointRequest.GetRequestStream());
+            writer.Write(stringData);
+            writer.Flush();
+
+            var endpointResponse = (HttpWebResponse)endpointRequest.GetResponse();
+            try
+            {
+                var webResponse = endpointRequest.GetResponse();
+                var webStream = webResponse.GetResponseStream();
+                var responseReader = new StreamReader(webStream);
+                var response = responseReader.ReadToEnd();
+                var jobj = JObject.Parse(response);
+                responseReader.Close();
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException("Inside create SHarpoint Plugin " + e.Message);
+            }
+
+        }
+    }
+
+}
---
+++ CreateSPSubsiteInCRM/CreateSPSubsiteInCRM/FolderStructure.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateSPSubsiteInCRM
+{
+    public class FolderStructure : IPlugin
+    {
+        #region Secure/Unsecure Configuration Setup
+        private string _secureConfig = null;
+        private string _unsecureConfig = null;
+
+        public FolderStructure(string unsecureConfig, string secureConfig)
+        {
+            _secureConfig = secureConfig;
+            _unsecureConfig = unsecureConfig;
+        }
+        #endregion
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = factory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                var entity = (Entity)context.InputParameters["Target"];
+
+                var urlString = @"sharePointUrl";
+                if (entity.LogicalName == "entityName" && entity.Contains("fieldName"))
+                {
+                    var rootRecord = entity.GetAttributeValue<string>("fieldName"); //Main or root record
+                    if (CreateSharepointSite.CreateSubsite(service, rootRecord, urlString))
+                        CreateSharePointSiteDocLocCRM(service, entity.Id, rootRecord, urlString);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+
+        public static void CreateSharePointSiteDocLocCRM(IOrganizationService service, Guid rootRecordId, string rootRecord, string siteUrl)
+        {
+            try
+            {
+                var newSiteUrl = siteUrl + "/" + rootRecord + "/";
+                var spSite = new Entity("sharepointsite");
+                spSite["name"] = rootRecord;
+                spSite["description"] = rootRecord;
+                spSite["absoluteurl"] = newSiteUrl;
+                var _spSiteId = service.Create(spSite);
+
+                if (_spSiteId != Guid.Empty)
+                {
+                    var spDocLoc = new Entity("sharepointdocumentlocation");
+                    spDocLoc["name"] = rootRecord + " Root Document Library";
+                    spDocLoc["description"] = rootRecord + " Root Document Library";
+                    spDocLoc["parentsiteorlocation"] = new EntityReference("sharepointsite", _spSiteId);
+                    spDocLoc["relativeurl"] = "sharepoinrUrl";
+                    var spDocLocCreate = service.Create(spDocLoc);
+
+                    if (spDocLocCreate != Guid.Empty)
+                    {
+                        var spDocLoc2 = new Entity("sharepointdocumentlocation");
+                        spDocLoc2["name"] = rootRecord;
+                        spDocLoc2["description"] = rootRecord + " Documents";
+                        spDocLoc2["parentsiteorlocation"] = new EntityReference("sharepointdocumentlocation", spDocLocCreate);
+                        spDocLoc2["relativeurl"] = "Documents";
+                        spDocLoc2["regardingobjectid"] = new EntityReference("rootEntityName", rootRecordId);
+                        var spDocLocCreate2 = service.Create(spDocLoc2);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
